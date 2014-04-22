b990841ae68ea0fb65776f7976c1197a3a32a9a7
Adrian Miranda <adrian.miranda@ppivot.com.br>
2014-04-22 14:04:42 -0300

    0.0.1

--- test/methods.js
+++ test/methods.js
@@ -1,1 +1,16 @@
 'use strict';
+
+var expect = chai.expect;
+
+describe('AM.Sprite', function () {
+    describe('constructor', function () {
+        it('should have a default element', function() {
+            var sprite = new AM.Sprite(null, {});
+            expect(sprite.element).to.equal(null);
+        });
+        it('should have a basic options', function() {
+            var sprite = new AM.Sprite(null);
+            expect(sprite.options).to.equal({});
+        });
+    });
+});
